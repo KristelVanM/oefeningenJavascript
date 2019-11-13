e66bf7a3c5a5c7766b76d580785a4a34eb48598d
kristel van mileghem <kristelvanmileghem@hotmail.com>
2019-11-13 14:17:33 +0100

    toevoeging challenge 02 math

--- 01-base/04-asv-confirm/script.js
+++ 01-base/04-asv-confirm/script.js
@@ -10,7 +10,8 @@
 // You will have time to focus on it later.
 
 
-/*
+/* 
+//begin:
 var askName = "Please enter your name";
 var askGender = "Please enter your gender";
 var askCity = "Please enter your city";
@@ -33,19 +34,73 @@ else {
 }
 */
 
+/*
+//tweede poging:
+function questions (){
 var askName = "Please enter your name";
 var askGender = "Please enter your gender";
 var askCity = "Please enter your city";
+
 
 var name = prompt (askName);
 var gender = prompt (askGender);
 var city = prompt (askCity);
 
 var a = `Your name is ${name}, your gender is ${gender} and you live in ${city}`;
-
 alert (a);
+}
 
-var ok = confirm ("Is this correct?");
+questions ();
+
+var askConfirm = "Is this correct?";
+var saySomething = confirm (askConfirm);
+
+
+function check() {
+    if ( ok ===true) {
+        alert ("Thank you");
+    }
+    else {
+        alert ("try again");
+        questions ();
+        
+    }
+
+}
+
+check();
+*/
+
+
+
+function onMyButtonClicked(){
+    console.log("My button was clicked");
+}
+var askName = "Please enter your name";
+var askGender = "Please enter your gender";
+var askCity = "Please enter your city";
+
+var name;
+var gender;
+var city;
+
+var a;
+
+var ok = false;
+
+while(ok == false){
+    name = prompt (askName);
+    gender = prompt (askGender);
+    city = prompt (askCity);
+
+    a = `Your name is ${name}, your gender is ${gender} and you live in ${city}`;
+
+    alert(a);
+
+    ok = confirm ("Is this correct?");
+    
+    check();
+}
 
 function check() {
     if ( ok ===true) {
@@ -57,7 +112,3 @@ function check() {
     }
 
 }
-
-check();
-
-   
